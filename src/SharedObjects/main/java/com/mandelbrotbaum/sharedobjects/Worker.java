d6ace8513b164a6cfb3a509f7549495676314e91
sebastiantress03 <SebastianTress@t-online.de>
2025-05-27 10:21:49 +0200

    fix again

---
+++ src/SharedObjects/main/java/com/mandelbrotbaum/sharedobjects/Worker.java
@@ -0,0 +1,8 @@
+package com.mandelbrotbaum.sharedobjects;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Worker extends Remote {
+    int[][] compute(int wPx, int hPx, double wR, double hR, double x0, double y0, int maxIterations) throws RemoteException;
+}
